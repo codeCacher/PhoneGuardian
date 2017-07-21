40041accfed866739f76c4ec5af63259b3e44eca
codeCacher <2423024562@qq.com>
2017-07-22 03:36:32 +0800

    SIM卡变更发送短信

---
+++ PhoneGuardian/app/src/main/java/com/cs/phoneguardian/guardian/receiver/BootCompleteReceiver.java
@@ -0,0 +1,36 @@
+package com.cs.phoneguardian.guardian.receiver;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.SystemClock;
+import android.telephony.SmsManager;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.cs.phoneguardian.main.MainActivity;
+import com.cs.phoneguardian.modle.PhoneStateDataSource;
+import com.cs.phoneguardian.utils.Constants;
+import com.cs.phoneguardian.utils.SharedPreferencesUtils;
+
+/**
+ * Created by Administrator on 2017/7/22.
+ */
+
+public class BootCompleteReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        boolean guardOpen = SharedPreferencesUtils.getBoolean(context, Constants.KEY_GUARD_OPEN_STATE, false);
+        String savedSIM = SharedPreferencesUtils.getString(context, Constants.KEY_SIM, "");
+        String SIM = PhoneStateDataSource.getInstance(context).getSimSerialNumber();
+        if ((!TextUtils.isEmpty(savedSIM)) && (!SIM.equals(savedSIM)) && guardOpen) {
+            //如果重启后SIM序列号与之前绑定序列号不同，则发送报警短信
+            String mergencyContactNumber = SharedPreferencesUtils.getString(context, Constants.KEY_MERGENCY_CONTACT, "");
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(mergencyContactNumber, null, "你的手机卡变更了！", null, null);
+        }
+    }
+}
--- PhoneGuardian/app/src/main/java/com/cs/phoneguardian/main/MainActivity.java
+++ PhoneGuardian/app/src/main/java/com/cs/phoneguardian/main/MainActivity.java
@@ -1,12 +1,19 @@
 package com.cs.phoneguardian.main;
 
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
